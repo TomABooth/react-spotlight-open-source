fb71cb25096d438c3a8d1b82d5110b2a02139fe6
Tom Booth <tombeezle@gmail.com>
2023-01-31 14:52:31 -0800

    css buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import table from './components/Table/table.js';
 import chart from './components/Chart/chart.js';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/table">Table</Link>
-        <Link to="/charts">Charts</Link>
+        <Button href="/table">Table</Button>
+        <Button href="/charts">Charts</Button>
       </nav>
       <Switch>
-        <Route path="/table" component={table} />
-        <Route path="/charts" component={chart} />
+        <Route exact path="/table" component={table} />
+        <Route exact path="/charts" component={chart} />
       </Switch>
     </div>
   );
